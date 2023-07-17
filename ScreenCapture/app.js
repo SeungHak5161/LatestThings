91155717647525ff68d80ecbc1e8c42b9bfc4fbd
SeungHak5161 <bsh93367@gmail.com>
2023-07-17 22:11:38 +0900

    linear-gradient(그라데이션), keyframe(애니메이션)

--- ScreenCapture/app.js
+++
@@ -1,9 +0,0 @@
-const previewElem = document.getElementById('preview');
-const startBtn = document.getElementById('start');
-
-startBtn.addEventListener("click", async () => {
-    previewElem.srcObject = await navigator.mediaDevices.getDisplayMedia({
-        video:{cursor:'always'},
-        audio:false,
-    });
-});
